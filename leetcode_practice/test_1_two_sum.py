1b5292e0b79cf76903d4702496855ae02d32fee9
marc.torsoc <marc.torsoc@gmail.com>
2024-12-04 13:13:25 +0100

    first commit

---
+++ leetcode_practice/test_1_two_sum.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    pass
+
+
+@pytest.mark.parametrize(
+    ("nums", "target", "expected"),
+    [
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
+    ],
+)
+def test_twoSum():
+    assert False
